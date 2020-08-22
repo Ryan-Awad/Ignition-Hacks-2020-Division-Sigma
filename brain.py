f6fa0cfb4d755f9989fd841653f3fc9004ec9e4d
Anusha Shekhar <anushashekhar@MacBook-Pro.local>
2020-08-22 21:37:09 +0530

    better organization

--- brain.py
+++ brain.py
@@ -1,14 +1,15 @@
 import pandas as pd
 import nltk
 from Algorithms.stopwords import use_stopwords
+from preprocessing import *
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-judgement_dataset = pd.read_csv("datasets/test.csv")
-
 sw_text_list = []
-for phrase in judgement_dataset.Text:
+for phrase in training_df.Text:
     sw_text = use_stopwords(phrase)
     sw_text_list.append(sw_text)
-    print("[Cleaned a text]")
+    # print(sw_text)
+
+preprocessing()
---
+++ preprocessing.py
@@ -0,0 +1,52 @@
+import nltk
+import re
+import numpy as np
+import pandas as pd
+import heapq
+
+training_df = pd.read_csv("datasets/test.csv")
+nltk.download('punkt')
+
+def preprocessing():
+
+    print(training_df)
+
+    for ind in range(len(training_df.index)):
+        text = training_df.loc[ind,"Text"] #str
+
+        ## tokenize each review
+        tokenized_phrase = nltk.sent_tokenize(text) #list
+
+        for i in range(len(tokenized_phrase)): #tokenized_phrase[i] is str
+            tokenized_phrase[i] = tokenized_phrase[i].lower()
+            tokenized_phrase[i] = re.sub(r'\W', ' ', tokenized_phrase[i])
+            tokenized_phrase[i] = re.sub(r'\s+', ' ', tokenized_phrase[i])
+            tokenized_phrase[i] = re.sub(r'\d+', ' ', tokenized_phrase[i])
+
+        # create bow from the tokenized list
+        bow = {} #dictionary to contain bag of words
+
+        for phrase in tokenized_phrase:
+            words = nltk.word_tokenize(phrase)
+            for word in words:
+                if word not in bow.keys():
+                    bow[word] = 1
+                else:
+                    bow[word] += 1
+
+        #take the top 10 words with the highest frequency from each phrase
+        freq_words = heapq.nlargest(10, bow, key=bow.get)
+
+        #assign a vector to each frequent word
+        freq_arr = []
+        for phrase in tokenized_phrase:
+            vector = []
+            for word in freq_words:
+                if word in nltk.word_tokenize(phrase):
+                    vector.append(1)
+                else:
+                    vector.append(0)
+            freq_arr.append(vector)
+        freq_arr = np.asarray(freq_arr)
+
+        print(freq_arr)
