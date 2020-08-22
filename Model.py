e80aa2a7f47d44082f3a052587e3010960061897
Robbot-syntax-error <68518446+Robbot-syntax-error@users.noreply.github.com>
2020-08-22 12:50:14 -0400

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 22 12:46:31 2020
+
+@author: wadid
+"""
+
+from sklearn import svm
+X = [[0, 0], [1, 1], [2, 2]]
+y = [0, 1, 2]
+clf = svm.SVC()
+clf.fit(X, y)
+
+print(clf.predict([[2, 2]]))
