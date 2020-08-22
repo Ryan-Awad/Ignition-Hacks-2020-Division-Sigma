1fe3b0dd97808de6f3a2e1de8154d64dd5d8aa21
Ryan Awad <61163520+Ryan-Awad@users.noreply.github.com>
2020-08-22 04:49:38 -0400

    Remade the use_stopwords function

--- brain.py
+++ brain.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import nltk
 from Algorithms.stopwords import use_stopwords
 
-judgement_dataset = pd.read_csv("datasets/contestant_judgment.csv")
-stopwords = use_stopwords(judgement_dataset)
+nltk.download('punkt')
+nltk.download('stopwords')
 
+judgement_dataset = pd.read_csv("datasets/contestant_judgment.csv") 
+
+sw_text_list = []
+for phrase in judgement_dataset.Text:
+    sw_text = use_stopwords(phrase)
+    sw_text_list.append(sw_text)
+    print("[Cleaned a text]")
