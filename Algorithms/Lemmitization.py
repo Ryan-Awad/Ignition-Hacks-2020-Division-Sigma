b065470edf05bdc5842a32e6205be99827f1b7ff
Robbot-syntax-error <68518446+Robbot-syntax-error@users.noreply.github.com>
2020-08-23 19:50:51 -0400

    Update Lemmitization.py

--- Algorithms/Lemmitization.py
+++ Algorithms/Lemmitization.py
@@ -4,10 +4,7 @@ Created on Sun Aug 23 12:57:52 2020
 
 @author: Robbot
 """
-
-
 # import these modules 
-from Algorithms.stopwords import use_stopwords
 import nltk
 import pandas as pd
 from ast import literal_eval
@@ -15,21 +12,12 @@ nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer 
 from nltk import wordnet
 
-nltk.download('punkt')
-training_df = pd.read_csv("training_data.csv")
-#print(training_df)
-
-lemmatizer = WordNetLemmatizer() 
-
-
-def lemoning():
+def lemoning(text):
+    lemmatizer = WordNetLemmatizer() 
     print("lemmatization time BABY!...")
-
-    sw_text_list = []
-    for phrase in training_df.Text:
-        sw_text = use_stopwords(phrase)
-        sw_text_list.append(sw_text)
-        
-    for i in range(len(sw_text_list)):
-        for h in range(len(sw_text_list[i])):
-            sw_text_list[i][h] = lemmatizer.lemmatize(sw_text_list[i][h])
+    sw_text = text
+    for i in range(len(text)):
+        for h in range(len(text[i])):
+            sw_text[i][h] = lemmatizer.lemmatize(sw_text[i][h])
+    return sw_text
+      
