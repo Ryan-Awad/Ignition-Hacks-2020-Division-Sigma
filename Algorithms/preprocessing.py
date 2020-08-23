76d3a284e997ddfd8d6da7b3286fe6c9e3fa942a
Anusha Shekhar <anushashekhar@MacBook-Pro.local>
2020-08-23 18:31:19 +0530

    fixed preprocessing method and clf.fit

--- Algorithms/preprocessing.py
+++ Algorithms/preprocessing.py
@@ -7,62 +7,58 @@ from ast import literal_eval
 from Algorithms.stopwords import use_stopwords
 
 nltk.download('punkt')
-training_df = pd.read_csv("datasets/test.csv")
+training_df = pd.read_csv("datasets/training_data.csv")
 
-
+# training_df = training_df[:10000]
 
 def preprocessing():
-
     print(training_df)
+    print("PREPROCESSING...")
+
     sw_text_list = []
     for phrase in training_df.Text:
         sw_text = use_stopwords(phrase)
         sw_text_list.append(sw_text)
         # print(sw_text)
-    for ind in range(len(training_df.index)):
-        # text = training_df.loc[ind,"Text"] #str
-        text = str(sw_text_list[ind])
 
-        ## tokenize each review
-        tokenized_phrase = nltk.sent_tokenize(text) #list
+    training_df['sw_text'] = sw_text_list
+    training_df['sw_text'] = training_df['sw_text'].str.lower().replace(regex=r'\W', value=' ').replace(regex=r'\s+', value=' ').replace(regex=r'\d+', value=' ')
 
-        for i in range(len(tokenized_phrase)): #tokenized_phrase[i] is str
-            tokenized_phrase[i] = tokenized_phrase[i].lower()
-            tokenized_phrase[i] = re.sub(r'\W', ' ', tokenized_phrase[i])
-            tokenized_phrase[i] = re.sub(r'\s+', ' ', tokenized_phrase[i])
-            tokenized_phrase[i] = re.sub(r'\d+', ' ', tokenized_phrase[i])
+    vectorized_phrases = []
+    training_sentiments = []
+    bow = {}
 
-        # create bow from the tokenized list
-        bow = {} #dictionary to contain bag of words
+    print("CREATING BOW...")
+    for ind, row in training_df.iterrows():
+        training_sentiments.append(row["Sentiment"])
 
-        for phrase in tokenized_phrase:
-            words = nltk.word_tokenize(phrase)
-            for word in words:
-                if word not in bow.keys():
-                    bow[word] = 1
-                else:
-                    bow[word] += 1
+        # create dictionary to contain bag of words from the tokenized list
+        for word in str(row['sw_text']):
+            if word not in bow.keys():
+                bow[word] = 1
+            else:
+                bow[word] += 1
 
-        #take the top 10 words with the highest frequency from each phrase
-        freq_words = heapq.nlargest(20, bow, key=bow.get)
+    print("VECTORIZING...")
+    # take the top 20 words with the highest frequency from each phrase
+    freq_words = heapq.nlargest(10000, bow, key=bow.get)
 
-        #assign a vector to each frequent word
-        freq_arr = []
-        for phrase in tokenized_phrase:
-            vector = []
-            for word in freq_words:
-                if word in nltk.word_tokenize(phrase):
-                    vector.append(1)
-                else:
-                    vector.append(0)
-            freq_arr.append(vector)
-        freq_arr = np.asarray(freq_arr)
+    #assign a vector to each frequent word
+    freq_arr = []
+    for phrase in training_df['sw_text']:
+        vector = []
+        for word in freq_words:
+            # if word in nltk.word_tokenize(phrase):
+            if word in str(phrase):
+                vector.append(1)
+            else:
+                vector.append(0)
+        freq_arr.append(vector)
+    vectorized_phrases = np.asarray(freq_arr)
 
-        freq_arr = freq_arr.tolist()
-        str1 = ' '.join(str(e) for e in freq_arr)
+    # vectorized_phrases.append(freq_arr)
 
-        training_df.at[ind, "Vectorized"] = str1
+    vectorized_phrases = np.asarray(vectorized_phrases)
 
-    print("_________________________________________")
-    print(training_df)
-    return training_df
+    # returns original read dataframe, array of sentiments, array of vectorized phrases
+    return training_df, vectorized_phrases, training_sentiments
--- brain.py
+++ brain.py
@@ -1,15 +1,66 @@
 import pandas as pd
 import nltk
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler, LabelBinarizer
+from sklearn.model_selection import train_test_split
 from Algorithms.stopwords import use_stopwords
 from Algorithms.preprocessing import *
 
-nltk.download('punkt')
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+
 nltk.download('stopwords')
 
-# sw_text_list = []
-# for phrase in training_df.Text:
-#     sw_text = use_stopwords(phrase)
-#     sw_text_list.append(sw_text)
-#     print(sw_text)
+# x is vectorized_phrases and y is training_sentiments
+training_df, x, y = preprocessing()
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=123)
 
-preprocessing()
+print("TRAINING...")
+clf = SVC(kernel='linear')
+clf.fit(x_train, y_train)
+
+accuracy = clf.score(x_test, y_test)
+
+print("ACCURACY: " + str(accuracy))
+
+
+# -----------------------------------------------
+
+# df = pd.read_csv("datasets/training_data.csv")
+#
+# x_train, x_test, y_train, y_test = train_test_split(df.Text,df.Sentiment,test_size=0.33,random_state=42) #splitting the dataset into training and testing
+#
+# #check the dimensions
+# print("Number of training samples: {}".format(x_train.shape[0]))
+# print("Number of testing samples: {}".format(x_test.shape[0]))
+#
+# #bag of words model to convert text to numbers
+# cv=CountVectorizer(binary=False)
+# print(type(cv))
+# print(cv)
+#
+# #transformed train reviews
+# train_reviews = cv.fit_transform(x_train)
+# #transformed test reviews
+# test_reviews = cv.transform(x_test)
+# print(type(train_reviews))
+# print(x_train)
+#
+# #labeling the sentient data
+# lb=LabelBinarizer()
+# #transformed sentiment data
+# sentiment_data = lb.fit_transform(y_train)
+# sentiment_test = lb.fit_transform(y_test)
+# print(sentiment_data.shape)
+#
+# #The data preparation is done, steps below prepare a sample model
+# #preparing the model
+# lr=LogisticRegression()
+# #training the model for Bag of words
+# lr_bow=lr.fit(train_reviews,sentiment_data)
+#
+# #Accuracy score for bag of words
+# lr_bow_predict = lr.predict(test_reviews)
+# lr_bow_score = accuracy_score(sentiment_test,lr_bow_predict)
+# print("lr_bow_score :",lr_bow_score)
