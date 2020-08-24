a446ad68b3102ffff895ea237dc659c2b27bd302
Robbot-syntax-error <68518446+Robbot-syntax-error@users.noreply.github.com>
2020-08-23 20:01:59 -0400

    Update segmentatory.py

--- Algorithms/segmentatory.py
+++ Algorithms/segmentatory.py
@@ -2,27 +2,15 @@ import nltk
 import pandas as pd
 from ast import literal_eval
 import morfessor
-from Algorithms.stopwords import use_stopwords
 
-
-nltk.download('punkt')
-training_df = pd.read_csv("training_data.csv")
-bruh_file_read = open("segment.txt", 'rb')
-model_file = pickle.load(bruh_file_read)
-
-
-io = morfessor.MorfessorIO()
-model = io.read_binary_model_file("model.bin")
-
-def segmenting():
+def segmenting(text):
     print("segmentation time BABY!...")
+    io = morfessor.MorfessorIO()
+    model = io.read_binary_model_file("model.bin")
 
-    sw_text_list = []
-    for phrase in training_df.Text:
-        sw_text = use_stopwords(phrase)
-        sw_text_list.append(sw_text)
 
-        
-    for i in range(len(sw_text_list)):
-        for h in range(len(sw_text_list[i])):
-          sw_text_list[i][h] = model.viterbi_segment(sw_text_list[i][h])[0][0]
+    sw_text = text
+    for i in range(len(sw_text)):
+        for h in range(len(sw_text[i])):
+          sw_text[i][h] = model.viterbi_segment(sw_text[i][h])[0][0]
+    return sw_text
