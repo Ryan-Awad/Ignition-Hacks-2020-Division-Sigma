3577e8a56773820b186058994cdaf70b899e3943
Ryan Awad <61163520+Ryan-Awad@users.noreply.github.com>
2020-08-23 23:25:58 -0400

    Added final dataset with sentiments

--- brain.py
+++ brain.py
@@ -23,53 +23,6 @@ print("TRAINING...")
 clf = SVC(kernel='rbf')
 clf.fit(x_train, y_train)
 
-print("PICKLING...")
-# to save an AI
-file_write = open("AI.txt", 'wb')
-pickle.dump(clf, file_write)
-file_write.close()
-
 accuracy = clf.score(x_test, y_test)
 print("F1_SCORE:", f1_score(y_test, clf.predict(x_test), average=None, labels=[1, 0]))
 print("ACCURACY: " + str(accuracy))
-
-
-# -----------------------------------------------
-
-# df = pd.read_csv("datasets/training_data.csv")
-#
-# x_train, x_test, y_train, y_test = train_test_split(df.Text,df.Sentiment,test_size=0.33,random_state=42) #splitting the dataset into training and testing
-#
-# #check the dimensions
-# print("Number of training samples: {}".format(x_train.shape[0]))
-# print("Number of testing samples: {}".format(x_test.shape[0]))
-#
-# #bag of words model to convert text to numbers
-# cv=CountVectorizer(binary=False)
-# print(type(cv))
-# print(cv)
-#
-# #transformed train reviews
-# train_reviews = cv.fit_transform(x_train)
-# #transformed test reviews
-# test_reviews = cv.transform(x_test)
-# print(type(train_reviews))
-# print(x_train)
-#
-# #labeling the sentient data
-# lb=LabelBinarizer()
-# #transformed sentiment data
-# sentiment_data = lb.fit_transform(y_train)
-# sentiment_test = lb.fit_transform(y_test)
-# print(sentiment_data.shape)
-#
-# #The data preparation is done, steps below prepare a sample model
-# #preparing the model
-# lr=LogisticRegression()
-# #training the model for Bag of words
-# lr_bow=lr.fit(train_reviews,sentiment_data)
-#
-# #Accuracy score for bag of words
-# lr_bow_predict = lr.predict(test_reviews)
-# lr_bow_score = accuracy_score(sentiment_test,lr_bow_predict)
-# print("lr_bow_score :",lr_bow_score)
